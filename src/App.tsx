73bf6c9af427bb622f1e7b5af7d2a3eec89160e8
Amit <amit@family-chores.local>
2026-04-03 19:56:46 +0300

    feat: wire RouterProvider and AuthProvider to app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+
 export default function App() {
-  return <div>משימות משפחתיות</div>
+  return <RouterProvider router={router} />
 }
